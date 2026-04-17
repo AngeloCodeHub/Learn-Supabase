5ca218120e72d9aac622803793db18268129abf2
AngeloHu <a0920217700@gmail.com>
2026-04-17 18:48:57 +0800

    docker uv

--- tools/Docker-uv.sh
+++ tools/Docker-uv.sh
@@ -3,4 +3,5 @@ exeComm="specify init --here --ignore-agent-tools"
 docker run -it --rm -w /home \
 	-v uv:/root \
 	-v ${PWD}:/home \
-	ghcr.io/astral-sh/uv:debian /bin/bash -i -c "$exeComm"
+	ghcr.io/astral-sh/uv:debian /bin/bash
+	# ghcr.io/astral-sh/uv:debian /bin/bash -i -c "$exeComm"
